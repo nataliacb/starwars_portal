57ffea0d5c87d7319f6653b35a6025019ff23805
Naty <nataliacb28@gmail.com>
2017-12-17 22:41:38 -0200

    Add Styles and Component Search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -18,27 +18,9 @@ import {
 })
 export class SearchComponent implements OnInit {
 
-  heroes$: Observable<Character[]>;
-  private searchTerms = new Subject<string>();
-
-  constructor(private heroService: CharacterService) {}
-
-  // Push a search term into the observable stream.
-  search(term: string): void {
-    this.searchTerms.next(term);
-  }
+  constructor() {}
 
   ngOnInit(): void {
-    this.heroes$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
-
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
-
-      // switch to new search observable each time the term changes
-      switchMap((term: string) => this.heroService.searchHeroes(term)),
-    );
   }
 
 }
